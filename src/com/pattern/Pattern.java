c3b693f67909cbe3fe567a70aca3cc0d1e9955a6
SHAMEER96 <shameer.ms110@gmail.com>
2023-02-26 12:29:53 +0530

    second program

---
+++ src/com/pattern/Pattern.java
@@ -0,0 +1,13 @@
+package com.pattern;
+
+public class Pattern {
+	public static void main(String args[]) {
+	int sh=5;
+	for(int i=0;i<=sh;i++) {
+for(int j=1;j<=i;j++) {
+	System.out.print(" * ");
+}
+System.out.println();
+}
+}
+}
